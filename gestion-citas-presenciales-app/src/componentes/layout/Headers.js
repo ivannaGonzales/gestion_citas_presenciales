8191c3c75de45f4d5fa42ee8664acde2e6da6265
ivannaGonzales <ic28011999>
2025-09-28 21:51:19 +0200

    [ISTA-0008]: Iniciacion del frontend

---
+++ gestion-citas-presenciales-app/src/componentes/layout/Headers.js
@@ -0,0 +1,15 @@
+
+
+const Header = () => {
+    return (
+        <header className="barra">
+            <div className="contenedor">
+                <h1>TFG - Gestión citas presenciales</h1>
+            </div>
+        </header>
+    )
+}
+
+
+
+export default Header;
