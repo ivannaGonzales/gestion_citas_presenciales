c474f2c3bd2416893d7e2fe700b2252623538a3b
ivannaGonzales <ic28011999>
2025-10-18 21:03:13 +0200

    [ISTA-0008]: Se suben los cambios sobre la pagina principal para que salga informacion sobre incidencias

--- gestion-citas-presenciales-app/src/componentes/incidencias/Incidencias.js
+++ gestion-citas-presenciales-app/src/componentes/incidencias/Incidencias.js
@@ -1,24 +1,40 @@
 
-import { useEffect } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import clienteAxios from '../../config/axios';
+import Incidencia from './Incidencia';
 //importar cliente axios
 function Incidencias() {
 
-    //query a la API
+    //trabajar con el state
+    //incidencias = state
+    // savaIncidencias = funcion para guardar el state
+    // lo guardamos en el state
+    const [incidencias, saveIncidencias] = useState([]);//valor inicial
 
     const consultarAPI = async () => {
-        console.log('Consultando ...')
         const getIncidencias = await clienteAxios.get('incidencias/empresa/Movistar');
+        //colocar el resultado en el state
+        saveIncidencias(getIncidencias.data);
 
-        console.log(getIncidencias)
     }
 
     //se carga automaticamente
     useEffect(() => {
         consultarAPI();
-    })
+    }, [])
     return (
-        <h2>Incidencias</h2>
+        <Fragment>
+            <h2>Incidencias</h2>
+
+            <ul className='listado-incidencias'>
+                {incidencias.map(incidencia =>
+                    <Incidencia
+                        incidencia={incidencia}
+                    />
+                )}
+            </ul>
+        </Fragment>
+
     )
 }
 export default Incidencias;
